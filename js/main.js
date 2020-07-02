423fca5b97298919daacbf42b08ae5cbd626a6f1
Tomomi <tomoiwamoto21@gmail.com>
2020-07-02 02:40:34 -0400

    my first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event){
+ if (!event.target.matches("#dropbutton")){
+     let dropdowns = document.getElementsByClassName("dropdown-content");
+     let i;
+     for (i=0; i<dropdowns.length; i++){
+         let openDropdown= dropdowns[i];
+         if (openDropdown.classList.contains("show")){
+             openDropdown.classList.remove("show");
+         }
+     }
+ }
+
+}
