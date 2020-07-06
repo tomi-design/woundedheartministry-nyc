8d880195085d59fc7e5e7aa565ec3b8c8bb644e1
Tomomi <tomoiwamoto21@gmail.com>
2020-07-06 03:36:46 -0400

    changed position of slideshow to the bottom of page. Made slide show images to be able to link to other pages.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+/*---For dropdown menu----*/
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -14,4 +17,25 @@ window.onclick = function(event){
      }
  }
 
+}
+
+
+
+let slideIndex=0;
+showSlides();
+
+function showSlides(){
+    let i;
+    let slides= document.getElementsByClassName('slideshow');
+    for (i=0; i < slides.length; i++){
+        slides[i].style.display="none";
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length){
+        slideIndex=1
+    }
+
+    slides[slideIndex-1].style.display="block";
+    setTimeout(showSlides, 5000); 
 }
